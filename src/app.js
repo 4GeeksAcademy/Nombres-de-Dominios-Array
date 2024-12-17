1a52c0210472cfb2b9a295f84c58d2044dede0eb
Roberto Agredo <memoriaderam@gmail.com>
2024-12-17 02:56:41 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,59 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+let pronoun = ["the", "our","your"];
+let adj = ["great", "big","excelent"];
+let noun = ["jogger", "racoon","randow"];
+let extencion = [".com", ".net",".es"];
+
+function Pronoun() {
+  let randomNumberp = Math.floor(Math.random() * 3);
+  if (randomNumberp == 0) {
+    return pronoun[0];
+  } else if(randomNumberp == 1) {
+    return pronoun[1];
+  } else if(randomNumberp == 2) {
+    return pronoun[2];
+  }
+}
+Pronoun();
+
+function Adj() {
+    let randomNumberp = Math.floor(Math.random() * 3);
+    if (randomNumberp == 0) {
+      return adj[0];
+    } else if(randomNumberp == 1) {
+      return adj[1];
+    } else if(randomNumberp == 2) {
+      return adj[2];
+    }
+  }
+Adj();
+
+function Noun() {
+    let randomNumberp = Math.floor(Math.random() * 3);
+    if (randomNumberp == 0) {
+      return noun[0];
+    } else if(randomNumberp == 1) {
+      return noun[1];
+    } else if(randomNumberp == 2) {
+      return noun[2];
+    }
+  }
+Noun();
+
+function Extencion() {
+    let randomNumberp = Math.floor(Math.random() * 3);
+    if (randomNumberp == 0) {
+      return extencion[0];
+    } else if(randomNumberp == 1) {
+      return extencion[1];
+    } else if(randomNumberp == 2) {
+      return extencion[2];
+    }
+  }
+Noun();
+
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// console.log("Hello Rigo from the console!");
+console.log(Pronoun(pronoun)+Adj(adj)+Noun(noun)+Extencion(extencion));
